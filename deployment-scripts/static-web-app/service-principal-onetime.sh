dcfadd369972786f0d70d0e9d65cf8e5a8fea4a9
vishnuvardhansankoti <shankotai@hotmail.com>
2026-05-03 06:51:26 -0500

    ok

--- deployment-scripts/static-web-app/service-principal-onetime.sh
+++ deployment-scripts/static-web-app/service-principal-onetime.sh
@@ -1,3 +1,15 @@
 az ad sp create-for-rbac --name "my-swa-deploy-sp" --role contributor \
   --scopes /subscriptions/d7b747bf-f824-4852-9c2b-145ec1c2f4a7/resourceGroups/product-pocs-rg \
   --sdk-auth
+
+az ad app list --display-name "my-swa-deploy-sp" --query "[0].id" -o tsv
+
+az ad app federated-credential create \
+  --id <APP_OBJECT_ID> \
+  --parameters '{
+    "name": "github-actions-production",
+    "issuer": "https://token.actions.githubusercontent.com",
+    "subject": "repo:vishnuvardhansankoti/talking-top-app:environment:production",
+    "description": "GitHub Actions deploy to production",
+    "audiences": ["api://AzureADTokenExchange"]
+  }'
